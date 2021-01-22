19c877d13e77402f570efcf3df1f96a52b50cf13
이나영 <bokiri16@gmail.com>
2021-01-22 18:36:35 +0900

    List.vue와 JoinSession.vue 페이지 연결

--- frontend/src/lib/constants.js
+++ frontend/src/lib/constants.js
@@ -1,21 +1,22 @@
 export default {
-    TITLE: 'SSAFY 개발블로그',
-    baseUrl: 'http://ec2-54-180-116-157.ap-northeast-2.compute.amazonaws.com:8888',
-    LS_KEY: {
-        USER_TOKEN: 'userToken',
+  TITLE: "SSAFY 개발블로그",
+  baseUrl:
+    "http://ec2-54-180-116-157.ap-northeast-2.compute.amazonaws.com:8888",
+  LS_KEY: {
+    USER_TOKEN: "userToken",
+  },
+  ERROR: {
+    FRONT_ERROR: "FE00",
+  },
+  URL_TYPE: {
+    USER: {
+      LOGIN: "login",
+      JOIN: "join",
+      JOINSESSION: "joinSession",
+      MYPAGE: "mypage",
     },
-    ERROR: { 
-        FRONT_ERROR: 'FE00' 
+    POST: {
+      MAIN: "main",
     },
-    URL_TYPE: {
-        USER: {
-            LOGIN: 'login',
-            JOIN: 'join', 
-            MYPAGE: 'mypage',
-        },
-        POST: {
-            MAIN : "main",
-        },
-    }
-}
-
+  },
+};
