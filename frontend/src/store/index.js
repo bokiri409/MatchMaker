cf0295de8cb3810cc2617d797170434c5c981dbb
이나영 <bokiri16@gmail.com>
2021-02-08 02:49:03 +0900

    feat : 로컬스토리지에 유저 정보 저장, 새로고침 시 로그인 유지

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -8,6 +8,7 @@ export default new Vuex.Store({
   state: {
     email: '',
     nickname: '',
+    token: '',
   },
   getters: {
     getEmail(state) {
@@ -18,15 +19,38 @@ export default new Vuex.Store({
     },
   },
   mutations: {
+    CHECKUSER(state, userData) {
+      state.email = userData.email;
+      state.nickname = userData.nickname;
+      state.token = userData.token;
+      //반환된 유저 정보를 로컬스토리지에 저장
+      localStorage.setItem("userData", JSON.stringify(userData));
+      //token(토큰)을 Axios Header에 추가
+      // Axios instance Authorization Header에 JWT Token을 포함
+      axios.defaults.headers.common["x-auth-token"] = `Bearer ${state.token}`;
+    },
     LOGIN(state, payload) { //로그인 성공 시
-      state.email = payload.object.email;
-      state.nickname = payload.object.nickname;
+        state.email = payload.object.email;
+        state.nickname = payload.object.nickname;
+        state.token = payload.data;
+        const userData = {
+          email: state.email,
+          nickname: state.nickname,
+          token: state.token
+        };
+        //반환된 토큰을 로컬스토리지에 저장
+        localStorage.setItem("userData", JSON.stringify(userData));
+        //token(토큰)을 Axios Header에 추가
+        // Axios instance Authorization Header에 JWT Token을 포함
+        axios.defaults.headers.common["x-auth-token"] = `Bearer ${state.token}`;
     },
     LOGOUT(state) {
       state.email = '';
       state.nickname = '';
+      state.token = '';
       // 로그아웃 시 저장된 토큰 삭제
-      localStorage.removeItem("access-token");
+      localStorage.removeItem("userData");
+      // reload 인자값 없을 때, 디폴트 false 처리 되어 새로고침 하면서 클라이언트 캐쉬에서 페이지를 불러옴 헤더 세팅값이 리셋됨
       location.reload();
     },
   },
@@ -35,17 +59,7 @@ export default new Vuex.Store({
     LOGIN(context, user) {
       return axios.post("http://localhost:8080/account/login", user)
       .then((response) => { //로그인 성공 시 토큰(data) 반환
-          //토큰을 헤더에 포함시켜서 유저 정보를 요청
-          let token = response.data.data;
-          console.log(token);
-          //반환된 토큰을 로컬스토리지에 저장
-          localStorage.setItem("access-token", token);
           context.commit('LOGIN', response.data);
-          console.log(response);
-      })
-      .catch(err => { //로그인 실패 시
-        alert("이메일과 비밀번호를 확인하세요.");
-        console.log(err);
       })
     },
     LOGOUT(context) {
