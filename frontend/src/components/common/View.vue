7408816043f45c2bd852bb02812e74f560d1e0c5
kimdonghyunk <apxww@naver.com>
2021-02-10 16:39:37 +0900

    feat: 메인 페이지 구현

---
+++ frontend/src/components/common/View.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <Welcome id="Home" />
+
+    <about-us id="aboutus" />
+
+    <recent-reviews id="reviews" />
+
+    <contact-us id="contact" />
+
+    <common-footer @gotolink="gotolink" />
+  </div>
+</template>
+
+<script>
+import AboutUs from '../../page/main/AboutUs.vue'
+import RecentReviews from '../../page/main/RecentReviews.vue'
+  export default {
+    name: 'CommonView',
+
+    components: {
+      ContactUs: () => import('../../page/main/ContactUs'),
+      RecentReviews: () => import('../../page/main/RecentReviews'),
+      // Services: () => import('@/components/Services'),
+      Welcome: () => import('../../page/main/Welcome'),
+      CommonFooter: () => import('../common/Footer'),
+      AboutUs: () => import('../../page/main/AboutUs'),
+    },
+    methods: {
+      gotolink (linkitem) {
+        this.$vuetify.goTo(`#${linkitem}`)
+      }
+    }
+  }
+</script>
