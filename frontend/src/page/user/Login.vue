466ffa0691ce582e20c340ed78641c8699f728cf
이나영 <bokiri16@gmail.com>
2021-02-17 02:23:23 +0900

    feat : 비밀번호 찾기(이메일 임시 비밀번호 전송) 기능 추가

--- frontend/src/page/user/Login.vue
+++ frontend/src/page/user/Login.vue
@@ -46,8 +46,8 @@
     <v-container fill-height style="max-width: 400px;" fluid>
         <v-layout align-center row wrap>
             <v-flex xs10>
-<!--                <v-card class="elevation-12">-->
-<!--                    <div class="pa-10">-->
+                <!--                <v-card class="elevation-12">-->
+                <!--                    <div class="pa-10">-->
                 <v-row
                     class="fill-height ma-0 pa-7"
                     align="center"
@@ -60,7 +60,7 @@
                         max-width="150"
                     ></v-img>
                 </v-row>
-                    <v-text-field
+                <v-text-field
                         label="email"
                         v-model="user.email"
                         id="email"
@@ -69,8 +69,8 @@
                         solo
                         rounded
                         dense
-                    ></v-text-field>
-                    <v-text-field
+                ></v-text-field>
+                <v-text-field
                         label="password"
                         v-model="user.password"
                         id="password"
@@ -83,92 +83,97 @@
                         :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
                         :type="show ? 'text' : 'password'"
                         @click:append="show = !show"
-                    ></v-text-field>
+                ></v-text-field>
 
-                    <v-btn
+                <v-btn
                         rounded
                         block
                         color="#F2ACC6"
                         @click="checkHandler()"
-                    >
-                        login
-                    </v-btn>
+                >
+                    login
+                </v-btn>
 
                 <p class="text-center pt-5">
                     아직 회원이 아니신가요?
-                <router-link
-                        v-bind:to="{ name: constants.URL_TYPE.USER.JOIN }"
-                        class="btn&#45;&#45;text"
-                >
-                    sign up
-                </router-link>
+                    <router-link
+                            v-bind:to="{ name: constants.URL_TYPE.USER.JOIN }"
+                            class="btn&#45;&#45;text"
+                    >
+                        sign up
+                    </router-link>
                 </p>
 
                 <p class="text-center">
-                    <a>비밀번호를 잊어버렸나요?</a>
+                    <router-link
+                        v-bind:to="{ name: constants.URL_TYPE.USER.FINDPASSWORD }"
+                        class="btn&#45;&#45;text"
+                    >
+                        비밀번호를 잊어버렸나요?
+                    </router-link>
                 </p>
-<!--                    </div>-->
-<!--                </v-card>-->
+                <!--                    </div>-->
+                <!--                </v-card>-->
             </v-flex>
         </v-layout>
     </v-container>
 </template>
 
 <script>
-import "../../assets/css/user.scss";
-import constants from "../../lib/constants";
+    import "../../assets/css/user.scss";
+    import constants from "../../lib/constants";
 
 
-export default {
-  components: {},
-  created() {},
-  watch: {},
-  methods: {
-    checkHandler: function() {
-      let err = true;
-      let msg = "";
+    export default {
+        components: {},
+        created() {},
+        watch: {},
+        methods: {
+            checkHandler: function() {
+                let err = true;
+                let msg = "";
 
-      //에러메세지 출력 - 이메일, 패스워드 입력 비었을 때
-      !this.user.email && ((msg = "이메일을 입력해주세요"), (err = false));
-      err &&
-        !this.user.password &&
-        ((msg = "패스워드를 입력해주세요"), (err = false));
-      err &&
-        !this.validEmail(this.user.email) &&
-        ((msg = "이메일 형식에 맞춰 입력해주세요 ex) abc@abc.com"),
-        (err = false));
+                //에러메세지 출력 - 이메일, 패스워드 입력 비었을 때
+                !this.user.email && ((msg = "이메일을 입력해주세요"), (err = false));
+                err &&
+                !this.user.password &&
+                ((msg = "패스워드를 입력해주세요"), (err = false));
+                err &&
+                !this.validEmail(this.user.email) &&
+                ((msg = "이메일 형식에 맞춰 입력해주세요 ex) abc@abc.com"),
+                    (err = false));
 
-      if (!err) alert(msg);
-      else this.login(); //err안걸리면 login함수 실행
-    },
-    validEmail: function(email) {
-      // 이메일 형식 체크
-      var re = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-Za-z0-9\-]+/;
-      return re.test(email);
-    },
-    login: function() {
-      this.$store.dispatch("LOGIN", this.user).then(() =>
-        this.$router.push({
-          path: "/",
-        })
-      )
-      .catch(() =>
-        alert("이메일과 비밀번호를 확인해주세요."),
-        this.user.email = '',
-        this.user.password = '',
-      );
-    },
-  },
-  data: () => {
-    return {
-      constants,
-      user: {
-        email: "",
-        password: "",
-      },
-    show: false,
-    password: 'Password'
+                if (!err) alert(msg);
+                else this.login(); //err안걸리면 login함수 실행
+            },
+            validEmail: function(email) {
+                // 이메일 형식 체크
+                var re = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-Za-z0-9\-]+/;
+                return re.test(email);
+            },
+            login: function() {
+                this.$store.dispatch("LOGIN", this.user).then(() =>
+                    this.$router.push({
+                        path: "/",
+                    })
+                )
+                .catch(() =>
+                        alert("이메일과 비밀번호를 확인해주세요."),
+                    this.user.email = '',
+                    this.user.password = '',
+                );
+            },
+        },
+        data: () => {
+            return {
+                constants,
+                user: {
+                    email: "",
+                    password: "",
+                },
+                show: false,
+                password: 'Password'
+            };
+        },
     };
-  },
-};
 </script>
